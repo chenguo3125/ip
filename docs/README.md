eb9e33cc240688ce65f7273a7f3c781afc6195ea
chen guo <guoc3125@gmail.com>
2025-08-20 23:40:36 +0800

    Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Milly User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Milly.java
@@ -0,0 +1,8 @@
+public class Milly {
+    public static void main(String[] args) {
+        String hi = " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " What can I do for you?\n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(hi);
+    }
+}
