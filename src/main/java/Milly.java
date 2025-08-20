73013c3800b810141a1bdd763feefc3a455a6e9c
chen guo <guoc3125@gmail.com>
2025-08-20 23:59:16 +0800

    Level-1

--- src/main/java/Milly.java
+++ src/main/java/Milly.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Milly {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String hi = " Hello! I'm [YOUR CHATBOT NAME]\n" +
-                " What can I do for you?\n"
-                + "Bye. Hope to see you again soon!";
+                " What can I do for you?\n";
         System.out.println(hi);
+
+        while (true) {
+            System.out.println("You: ");
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Milly: \nBye~ See u soon :DD");
+                break;
+            } else {
+                System.out.println("Milly: \n" + input);
+            }
+        }
     }
 }
