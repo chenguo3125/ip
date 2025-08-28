c6eb22505b2a4124d90503345e7cef215790ce17
chen guo <guoc3125@gmail.com>
2025-08-28 23:53:35 +0800

    Add the DateTime and update the comments

--- src/main/java/ToDos.java
+++ src/main/java/ToDos.java
@@ -1,3 +1,8 @@
+/**
+ * Represents a Deadline task with title and completion status.
+ * This is a concrete class inherited from the abstract Task class.
+ */
+
 public class ToDos extends Task {
     public ToDos(String title, boolean done) {
         super(title, done);
